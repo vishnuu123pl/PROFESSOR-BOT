97b529734fc8ed654d5c4f167fef3ca7118f7bcd
Vishnu1238881 <128618742+Vishnu1238881@users.noreply.github.com>
2023-04-24 11:24:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEdGi9kKW2rxtDGLmyiQqc_kTv_C9M9ZQAC-wkAAgx0QFdSVcV16VKtgx4E")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -98,7 +98,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEdGi9kKW2rxtDGLmyiQqc_kTv_C9M9ZQAC-wkAAgx0QFdSVcV16VKtgx4E")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
